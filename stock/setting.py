4bad6870b563f2dbd6af919e9e06e18d1fc71fe6
yufeng <yufeng0528@126.com>
2018-03-29 10:37:05 +0800

    换个名字，路径

---
+++ stock/setting.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+settings = {
+          "MONGO_SERVER": "localhost",
+          "MONGO_PORT": 27017,
+          "MONGO_DB": "testdb",
+          "MONGO_TABLE": "stock_history_d"
+          }
--- stock/stock_history.py
+++ stock/stock_history.py
@@ -3,28 +3,24 @@
 import tushare as ts
 import pymongo
 import json
-
-settings={
-          "MONGO_SERVER": "localhost",
-          "MONGO_PORT": 27017,
-          "MONGO_DB": "testdb",
-          "MONGO_TABLE": "stock_history_d"
-          }
+from setting import settings
 
 def test():
     df = ts.get_hist_data("510050")
     print(df)
 
 
-def get_history():
-    df = ts.get_k_data(code="600100", start="2009-01-01", end="2010-01-01", ktype="D", autype="hfq", index=False, retry_count=3, pause=0.001)
+def get_history(code=None, start=None, end=None):
+    df = ts.get_k_data(code=code, start=start, end=end, ktype="D", autype="hfq", index=False, retry_count=3, pause=0.001)
     data = json.loads(df.to_json(orient='records'))
+    print data
 
     mongo_client = pymongo.MongoClient(settings['MONGO_SERVER'], settings['MONGO_PORT'])
     db = mongo_client[settings['MONGO_DB']]
     table = db[settings['MONGO_TABLE']]
     table.insert(data)
+    print code,  "from", start, "to", end, "存入数据库"
 
 
 if __name__ == "__main__":
-    get_history()
+    get_history('600100', '2009-01-01', '2009-02-01')
