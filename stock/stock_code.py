b0831e600c468d8f37b858a537f3e3b9dbfefb05
yufeng <yufeng0528@126.com>
2018-03-29 14:29:49 +0800

    修改后放入数据库

--- stock/stock_code.py
+++ stock/stock_code.py
@@ -12,22 +12,19 @@ def init_stock_code():
     :return:
     """
     df = ts.get_stock_basics()
-    #
-    # df.reset_index(level=0, inplace=True)
-    # print df
-
+    # 重设索引
     data = json.loads(df.reset_index().to_json(orient='records'))
-    f = file('log3.txt', "wb")
-    f.write(json.dumps(data))
-    f.close()
+    # f = file('log3.txt', "wb")
+    # f.write(json.dumps(data))
+    # f.close()
 
     # print data
-    #df.to_csv(path_or_buf='d/log.txt')
+    # df.to_csv(path_or_buf='d/log.txt')
 
-    # mongo_client = pymongo.MongoClient(settings['MONGO_SERVER'], settings['MONGO_PORT'])
-    # db = mongo_client[settings['MONGO_DB']]
-    # table = db[settings['MONGO_TABLE_STOCK_BASIC']]
-    # table.insert(data)
+    mongo_client = pymongo.MongoClient(settings['MONGO_SERVER'], settings['MONGO_PORT'])
+    db = mongo_client[settings['MONGO_DB']]
+    table = db[settings['MONGO_TABLE_STOCK_BASIC']]
+    table.insert(data)
     print "股票代码存入数据库"
 
 
