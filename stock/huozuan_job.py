73834248f7c56e55cf28b2ae37d05a0940e17ad1
yufeng <yf@huojutech.com>
2018-05-03 16:43:38 +0800

    No handlers could be found for logger "apscheduler.scheduler"

--- stock/huozuan_job.py
+++ stock/huozuan_job.py
@@ -4,7 +4,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime
 from stock_today_all import *
 from stock_history import *
+import logging
 
+
+logging.basicConfig()
 sched = BlockingScheduler()
 
 
@@ -16,12 +19,12 @@ def stock_runtime_job():
     print("stock_runtime_job end<<<")
 
 
-@sched.scheduled_job('date', day_of_week='mon-fri', hour='17', minute='5')
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour='17', minute='5')
 def stock_history_job():
     print datetime.datetime.now()
-    print("stock_runtime_job start>>>")
+    print("stock_history_job start>>>")
     get_last_day()
-    print("stock_runtime_job end<<<")
+    print("stock_history_job end<<<")
 
 sched.start()
 
