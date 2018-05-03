7dcc979409cf12c140aa70f3e692acd3bb8db9f7
yufeng <yf@huojutech.com>
2018-05-03 16:52:57 +0800

    去掉自己的日志

--- stock/huozuan_job.py
+++ stock/huozuan_job.py
@@ -19,18 +19,13 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour='9-16', minute='0-59', second='*/30')
 def stock_runtime_job():
-    print datetime.datetime.now()
-    print("stock_runtime_job start>>>")
     get_today_all()
-    print("stock_runtime_job end<<<")
 
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour='17', minute='5')
 def stock_history_job():
-    print datetime.datetime.now()
-    print("stock_history_job start>>>")
     get_last_day()
-    print("stock_history_job end<<<")
+
 
 sched.start()
 
