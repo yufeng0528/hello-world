88637a44be6849727a139d535751af4218892d84
yufeng <yf@huojutech.com>
2018-05-03 17:20:48 +0800

    继续测试

--- stock/huozuan_job.py
+++ stock/huozuan_job.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 import datetime
 from stock_today_all import *
 from stock_history import *
@@ -14,7 +14,7 @@ fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
 h = logging.StreamHandler()
 h.setFormatter(fmt)
 log.addHandler(h)
-sched = BlockingScheduler()
+sched = BackgroundScheduler()
 sched.daemonic = False
 
 
